083f5f2bdc344971d4c9a5570d167ebd8218419e
Gianluca Casati <fibo@users.noreply.github.com>
2012-10-13 03:00:33 +0200

    stay minimal

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ var Node = require('./lib/Node.js');
 
 exports.Node = Node;
 
+var Edge = require('./lib/Edge.js');
+
+exports.Edge = Edge;
+
+var Edge = require('./lib/Edge.js');
+
+exports.Edge = Edge;
+
+var Graph = require('./lib/Graph.js');
+
+exports.Graph = Graph;
+
+//Create a global dflow graph, a.k.a root.
+var dflow = {};
+dflow.root = new Graph();
+process.dflow = dflow;
+
---
+++ test/Graph.js
@@ -0,0 +1,15 @@
+
+var assert = require('assert');
+//var EventEmitter = require('events').EventEmitter;
+
+var dflow = require('../index.js');
+
+var Graph = dflow.Graph;
+
+describe('Graph', function () {
+  it('requires task', function () {
+    //var node = new Node();
+    //assert.throws;
+  });
+});
+
