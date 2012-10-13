52da30b07ea7225720f4a412ea570d7a003018ae
Gianluca Casati <fibo@users.noreply.github.com>
2012-10-13 04:45:52 +0200

    task is a function

--- test/Node.js
+++ test/Node.js
@@ -8,14 +8,25 @@ var Node = w2.Node;
 
 describe('Node', function () {
   describe('constructor:', function () {
-    it('requires task', function () {
-      //var node = new Node();
-      //assert.throws;
+    //var node = new Node({task:dummy});
+    it('requires a task function', function () {
+      var newNode1 = function () { new Node(); }
+      assert.throws(newNode1, Error);
+
+      var newNode2 = function () { new Node({}); }
+      assert.throws(newNode2, Error);
+
+      var newNode3 = function () { new Node({task:'foo'}); }
+      assert.throws(newNode3, Error);
+
+      var dummy = function () {};
+      var newNode4 = function () { new Node({task:dummy}); }
+      assert.doesNotThrow(newNode4, Error);
     });
   });
 
   describe('inheritance:', function () {
-    it('EventEmitter', function () {
+    it('is an EventEmitter', function () {
       var node = new Node({task:function(){}});
       assert.ok(node instanceof EventEmitter);
     });
--- test/Slot.js
+++ test/Slot.js
@@ -4,13 +4,14 @@ var assert = require('assert');
 
 var dflow = require('../index.js');
 
-var Graph = dflow.Graph;
+var Slot = dflow.Slot;
 
-describe('Graph', function () {
-  var graph = new Graph();
+describe('Slot', function () {
   it('requires task', function () {
     //var node = new Node();
     //assert.throws;
   });
 });
+
+
 
