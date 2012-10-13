3a1f7fe00622c17576ee433d3943376fd2cbb0be
Gianluca Casati <fibo@users.noreply.github.com>
2012-10-13 03:35:56 +0200

    added io

---
+++ test/In.js
@@ -0,0 +1,16 @@
+
+var assert = require('assert');
+//var EventEmitter = require('events').EventEmitter;
+
+var dflow = require('../index.js');
+
+var In = dflow.In;
+
+describe('In', function () {
+  it('requires task', function () {
+    //var node = new Node();
+    //assert.throws;
+  });
+});
+
+
---
+++ test/Out.js
@@ -0,0 +1,17 @@
+
+var assert = require('assert');
+//var EventEmitter = require('events').EventEmitter;
+
+var dflow = require('../index.js');
+
+var Out = dflow.Out;
+
+describe('Out', function () {
+  it('requires task', function () {
+    //var node = new Node();
+    //assert.throws;
+  });
+});
+
+
+
