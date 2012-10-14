2f15b74290e175e7f8168936e62a0128acc0dd0b
fibo <casati_gianluca@yahoo.it>
2012-10-14 20:59:27 +0200

    dummy test coverage

--- test/Graph.js
+++ test/Graph.js
@@ -7,10 +7,22 @@ var dflow = require('../index.js');
 var Graph = dflow.Graph;
 
 describe('Graph', function () {
-  var graph = new Graph();
-  it('requires task', function () {
-    //var node = new Node();
-    //assert.throws;
+  describe('constructor:', function () {
+    it('requires no argument', function () {
+      var graph = new Graph();
+      assert.ok(graph instanceof Graph);
+    });
+
+    it('nodes defaults to []', function () {
+      var graph = new Graph();
+      assert.deepEqual(graph.getNodes(), []);
+    });
+  });
+
+  describe('getNodes()', function () {
+  });
+
+  describe('addNode(<Node>)', function () {
   });
 });
 
--- test/In.js
+++ test/In.js
@@ -5,12 +5,34 @@ var assert = require('assert');
 var dflow = require('../index.js');
 
 var In = dflow.In;
+var Slot = dflow.Slot;
 
 describe('In', function () {
-  it('requires task', function () {
-    //var node = new Node();
-    //assert.throws;
+  describe('constructor:', function () {
+    it('', function () {
+    });
+  });
+
+  describe('inheritance:', function () {
+    it('is a Slot', function () {
+      var _in = new In();
+      assert.ok(_in instanceof Slot);
+    });
+  });
+
+  describe('getSource()', function () {
+    it('', function () {
+    });
+  });
+
+  describe('getData()', function () {
+    it('', function () {
+    });
+  });
+
+  describe('setData()', function () {
+    it('', function () {
+    });
   });
 });
-
 
