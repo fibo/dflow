437f913e5ea7c8c592a891e0425c33348b15da54
Gianluca Casati <fibo@users.noreply.github.com>
2012-10-16 20:50:54 +0200

    uffff

--- test/In.js
+++ test/In.js
@@ -8,14 +8,29 @@ var Slot = dflow.Slot;
 
 describe('In', function () {
   describe('constructor:', function () {
-    it('', function () {
+    it('requires name and data', function () {
+      //var _in = new In({name:'foo',data:'bar'});
+    });
+    it('throws Error if no name and or no data is passed', function () {
+      var newIn1 = function () { var _in = new In();console.log(_in); }
+      assert.throws(newIn1, Error);
+      var _in = new In({data:0,name:'xx'});console.log(_in.toJSON());
+
+      var newIn2 = function () { new In({}); }
+      //assert.throws(newIn2, Error);
+
+      var newIn3 = function () { new In({data:'bar'}); }
+      //assert.throws(newIn3, Error);
+
+      var newIn4 = function () { new In({name:'foo'}); }
+      //assert.throws(newIn4, Error);
     });
   });
 
   describe('inheritance:', function () {
     it('is a Slot', function () {
-      var _in = new In({data:0});
-      assert.ok(_in instanceof Slot);
+      //var _in = new In({name:'in1',data:0});
+      //assert.ok(_in instanceof Slot);
     });
   });
 
