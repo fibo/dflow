e37d4b27ac8f2b9fdee35622af210c6506aeecd2
fibo <casati_gianluca@yahoo.it>
2013-06-27 16:35:13 +0100

    added client

--- app.js
+++ app.js
@@ -3,65 +3,49 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes  = require('./routes');
-var http    = require('http');
-var path    = require('path');
-var sio     = require('socket.io');
+var fs      = require('fs')
+  , express = require('express')
+  , http    = require('http')
+  , path    = require('path')
 
-var app = express();
+var app = express()
 
 // Create process.dflow global object.
-require('./index.js');
+//require('./index.js');
 
-var df = process.dflow;
+//var df = process.dflow;
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+var clientDirPath = path.join(__dirname, 'client')
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+app.configure(function configureExpress(){
+  app.set('port', process.env.PORT || 3000)
+  app.set('views', __dirname + '/views')
+  app.use(express.favicon())
+  app.use(express.logger('dev'))
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+  //app.use(app.router)
+  app.use(express.static(path.join(__dirname, 'client')))
+})
 
-app.get('/', routes.index);
+app.configure('development', function configureExpressDevelopment() {
+  app.use(express.errorHandler())
+})
+
+app.get('/', function getRoot(req, res) {
+  fs.readFile(
+    path.join(__dirname, 'client', 'index.html'),
+    function(err, data) {
+      res.send(data)
+    }
+  )
+})
 
 //app.get('/graph/:id', routes.graph);
 
-var server = http.createServer(app);
+/**
+ * Start Application.
+ */
 
-server.listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
-
-var io = sio.listen(server);
-
-var doNothing = function () {}
-
-var _socket = io.sockets.on('connection', function (socket) {
-  socket.on('draw', function (clientId, draw) {
-    console.log(clientId); // YES it works
-    var rootToJSON = df.root.toJSON();
-    draw(rootToJSON);
-  });
-
-  socket.on('addNode', function (arg) {
-    var node = process.dflow.root.addNode(arg);
-    var nodeToJSON = node.toJSON();
-    socket.emit('addNode', nodeToJSON);
-    socket.broadcast.emit('addNode', nodeToJSON);
-  });
-
-  socket.on('disconnect', doNothing);
-});
+http.createServer(app).listen(app.get('port'))
 
