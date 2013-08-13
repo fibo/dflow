ccf2846019268fec34a50e5505696ec76df2de1c
Gianluca Casati <fibo@users.noreply.github.com>
2013-08-12 22:42:19 -0700

    added scenario and module

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 // Keep dflow as minimal as possible. Less is more.
 
+var iper = require('iper')
+
+var IperGraph = iper.IperGraph
+
+var root = new IperGraph()
+
+exports.root = root
+
 exports.DflowEdge  = require('./lib/DflowEdge.js')
 exports.DflowGraph = require('./lib/DflowGraph.js')
 exports.DflowNode  = require('./lib/DflowNode.js')
