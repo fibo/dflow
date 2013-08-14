a58d363254c18bb24cd1176d5f47f27bdb708bce
Gianluca Casati <fibo@users.noreply.github.com>
2013-08-14 16:25:00 -0700

    fixing hello world

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ exports.DflowGraph    = require('./lib/DflowGraph')
 exports.DflowInput    = require('./lib/DflowInput')
 exports.DflowOutput   = require('./lib/DflowOutput')
 exports.DflowScenario = require('./lib/DflowScenario')
+exports.DflowSlot     = require('./lib/DflowSlot')
 exports.DflowTask     = require('./lib/DflowTask')
 
 var dflowRoot = require('./lib/dflowRoot')
