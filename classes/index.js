4e28d3789286185b4a744946f5dbdd286cd88948
Gianluca Casati <fibo@users.noreply.github.com>
2014-02-24 15:04:21 +0100

    gulpified

--- classes/index.js
+++
@@ -1,9 +0,0 @@
-
-exports.DflowEdge    = require('./DflowEdge')
-exports.DflowGraph   = require('./DflowGraph')
-exports.DflowHost    = require('./DflowHost')
-exports.DflowInput   = require('./DflowInput')
-exports.DflowOutput  = require('./DflowOutput')
-exports.DflowPackage = require('./DflowPackage')
-exports.DflowTask    = require('./DflowTask')
-
