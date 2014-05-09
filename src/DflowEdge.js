8827c4230cd4a50aaef7ad045ec356b248d54245
Gianluca Casati <fibo@users.noreply.github.com>
2014-05-09 12:27:29 +0200

    better docs

--- src/DflowEdge.js
+++ src/DflowEdge.js
@@ -1,8 +1,20 @@
 
+/**
+ * Connect outputs to inputs
+ *
+ * @param {Object} source
+ * @param {Object} target
+ */
+
 function DflowEdge (source, target) {
   this.source = source
+
   this.target = target
 }
+
+/*!
+ * Export
+ */
 
 module.exports = DflowEdge
 
