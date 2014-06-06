4bc7ed421f01e3317ed0cb916b4ab99c411560b9
fibo <casati_gianluca@yahoo.it>
2014-06-06 07:40:38 +0200

    can read props from source tasks

---
+++ examples/graphs/nameOfTask.js
@@ -0,0 +1,21 @@
+{
+  "tasks": [
+    {
+      "id": 1,
+      "arg": [],
+      "name": "Math.PI"
+    },
+    {
+      "id": 2,
+      "arg": ["Task has name"],
+      "name": "console.log"
+    }
+  ],
+  "pipes": [
+    {
+      "id": 4,
+      "from": [1, "name"],
+      "to": [2, 1]
+    }
+  ]
+}
