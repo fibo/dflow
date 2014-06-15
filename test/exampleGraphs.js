9ef989bca2897ac615a458be94c96b61c4bbcfb3
Gianluca Casati <fibo@users.noreply.github.com>
2014-06-15 13:43:18 +0200

    test expected result from graphs

--- test/exampleGraphs.js
+++ test/exampleGraphs.js
@@ -2,12 +2,23 @@
 var dflow = require('../index')
   , fs    = require('fs')
   , path  = require('path')
+  , _     = require('underscore')
+
+var resultOf = {
+//  'graph1': 'ciao 5', TODO why result is 'ciao -1' ?
+  'graph2': 'The typeof Math.PI is number',
+  'nameOfTask': 'Task has name Math.PI',
+  'orOperator': 'true',
+  'dotOperator': '3.141592653589793'
+}
 
 fs.readdir(path.join('examples', 'graphs'), function (err, files) {
   if (err) throw err
 
   files.forEach(function (file) {
-    describe('example graph ' + file, function () {
+    var example = path.basename(file, '.json')
+
+    describe('example graph ' + example, function () {
       var graph
 
       it('require', function () {
@@ -15,12 +26,22 @@ fs.readdir(path.join('examples', 'graphs'), function (err, files) {
       })
 
       it('evaluate', function () {
-        // silent console
+        var expected = resultOf[example]
+
+        // silent oonsole
         var dir = global.console.dir
           , log = global.console.log
 
         dflow.register('console.dir', function () {})
         dflow.register('console.log', function () {})
+
+        // Inject a fake console.log to check the expected result from an example
+        if (typeof expected !== 'undefined') {
+          dflow.register('console.log', function () {
+            var result = _.toArray(arguments).join(' ')    
+            result.should.eql(expected)
+          })
+        }
 
         dflow.evaluate(graph)
 
