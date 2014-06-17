3d824f6e93e1a0a745f6ecb69ac69f56e633ae16
fibo <casati_gianluca@yahoo.it>
2014-06-17 16:49:42 +0200

    arrange layout

--- test/algorithm.js
+++ test/algorithm.js
@@ -2,6 +2,10 @@
 var dflow = require('../index')
 
 var emptyGraph = new dflow.Graph()
+  , emptyLayout = {
+    boxes: [],
+    lines: []
+  }
 
 describe('algorithm', function () {
   describe('evaluate', function () {
@@ -12,7 +16,26 @@ describe('algorithm', function () {
     })
   })
 
+  describe('arrange', function () {
+    it('return an empty layout for an empty graph', function () {
+      dflow.arrange(emptyGraph).should.eql(emptyLayout)
+    })
+
+    it('sort out a graph layout', function () {
+      var graph = require('../examples/graphs/orOperator.json')
+
+      var layout = dflow.arrange(graph)
+
+      layout.boxes.should.be.Array
+      layout.boxes.length.should.eql(graph.tasks.length)
+
+      layout.lines.should.be.Array
+      layout.lines.length.should.eql(graph.pipes.length)
+    })
+  })
+
   describe('levelOfTask', function () {
+    it('assigns a level to each task')
   })
 })
 
