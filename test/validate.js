45dd93bbd3e2cfd54200361133c06d82dd5b81d2
Gianluca Casati <fibo@users.noreply.github.com>
2014-10-16 16:31:12 +0000

    check for orphan pipes

--- test/validate.js
+++ test/validate.js
@@ -11,11 +11,15 @@ var funcs = {
   'f': Function.prototype
 }
 
+var notArray = /Not an Array:/
+  , notFunction = /Not a Function:/
+  , orphanPipe = /Orphan pipe:/
+
 describe('validate', function () {
   it('throws if some prop of funcs is not a function', function () {
     ;(function () {
       validate({'foo': 'bar'}, graph)
-    }).should.throwError(/Not a Function:/)
+    }).should.throwError(notFunction)
   })
 
   it('throws if graph.tasks is not an Array', function () {
@@ -26,7 +30,7 @@ describe('validate', function () {
 
     ;(function () {
       validate(funcs, graph)
-    }).should.throwError(/Not an Array:/)
+    }).should.throwError(notArray)
   })
 
   it('throws if graph.pipes is not an Array', function () {
@@ -37,7 +41,41 @@ describe('validate', function () {
 
     ;(function () {
       validate(funcs, graph)
-    }).should.throwError(/Not an Array:/)
+    }).should.throwError(notArray)
+  })
+
+  it('throws if a task has not a valid func', function () {
+    graph = {
+      tasks: [
+        {
+          'id': '1',
+          'func': 'not a function'
+        }
+      ],
+      pipes: []
+    }
+
+    ;(function () {
+      validate(funcs, graph)
+    }).should.throwError(notFunction)
+  })
+
+  it('throws if a pipe is orphan', function () {
+    graph = {
+      tasks: [
+      ],
+      pipes: [
+        {
+          'id': '1',
+          'from': { id: '2' },
+          'to': { id: '3', arg: 0 },
+        }
+      ]
+    }
+
+    ;(function () {
+      validate(funcs, graph)
+    }).should.throwError(orphanPipe)
   })
 })
 
