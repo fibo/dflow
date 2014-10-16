d45683db75585e9615979f9774ae433daedb8120
fibo <casati_gianluca@yahoo.it>
2014-10-16 16:31:12 +0000

    check for orphan pipes

--- src/validate.js
+++ src/validate.js
@@ -10,7 +10,34 @@ function validate (funcs, graph) {
   if (!Array.isArray(graph.tasks))
     throw new TypeError('Not an Array:', 'graph.tasks', graph.tasks)
 
-  graph.tasks.forEach(function () {})
+  function checkFunc (task) {
+    var f = funcs[task.func]
+
+    if (typeof f !== 'function')
+      throw new TypeError('Not a Function:', task.func, f)
+  }
+
+  graph.tasks.forEach(checkFunc)
+
+  function checkOrphan (pipe) {
+    var isOrphan = true
+
+    graph.tasks.forEach(function (task) {
+      if (!isOrphan)
+        return
+
+      if (pipe.from.id === task.id)
+        isOrphan = false
+
+      if (pipe.to.id === task.id)
+        isOrphan = false
+    })
+
+    if (isOrphan)
+      throw new TypeError('Orphan pipe:', pipe)
+  }
+
+  graph.pipes.forEach(checkOrphan)
 }
 
 module.exports = validate
