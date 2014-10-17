9f42ccf0266880135e95a75b48918cde4142b589
fibo <casati_gianluca@yahoo.it>
2014-10-17 20:18:13 +0000

    dflow.func returns

--- test/func.js
+++ test/func.js
@@ -3,12 +3,16 @@ var should = require('should')
   , func = require('../src/func')
 
 var graph = {
-  pipes: [],
+  pipes: [
+    { id: 'a', from: { id: '0' }, to: { id: '2', arg: 0 } },
+    { id: 'b', from: { id: '1' }, to: { id: '2', arg: 1 } },
+    { id: 'c', from: { id: '2' }, to: { id: '3', arg: 0 } }
+  ],
   tasks: [
-    {
-      id: '1',
-      func: '+'
-    }
+    { id: '0', func: 'arguments[0]' },
+    { id: '1', func: 'arguments[1]' },
+    { id: '2', func: '+' },
+    { id: '3', func: 'return' }
   ]
 }
 
@@ -16,19 +20,16 @@ var funcs = {
   '+': function (a, b) { return a + b }
 }
 
+var f = func(funcs, graph)
+
 describe('func', function () {
   it('returns a function', function () {
-    var f = func(funcs, graph)
     f.should.be.instanceOf(Function)
-    f()
+  f()
   })
 
-  it('validates funcs and graph', function () {
-    funcs.foo = 'bar'
-
-    ;(function () {
-      func(funcs, graph)
-    }).should.throwError()
+  it('function returns some value', function () {
+    f(1, 2).should.eql(3)
   })
 })
 
