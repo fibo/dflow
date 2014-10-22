fc34452ee999db8c0f4839eb91b63855bcd93ca4
Gianluca Casati <fibo@users.noreply.github.com>
2014-10-22 16:03:10 +0000

    added example with Math context

---
+++ test/examples/math.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+var dflow = require('dflow')
+
+var graph = {
+  task: {
+    '0': { func: 'arguments[0]' },
+    '1': { func: 'cos' },
+    '2': { func: 'return' }
+  },
+  pipe: {
+    '3': { from: '0', to: '1', arg: 0 },
+    '4': { from: '1', to: '2', arg: 0 }
+  }
+}
+
+var f = dflow.func(Math, graph)
+
+console.log(f(0.5))
+
