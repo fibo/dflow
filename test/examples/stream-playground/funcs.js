d04fe2bb9954d114bbb78f0c0ddab9b19c348035
fibo <casati_gianluca@yahoo.it>
2014-11-11 12:11:09 +0000

    added on end event

--- test/examples/stream-playground/funcs.js
+++ test/examples/stream-playground/funcs.js
@@ -5,11 +5,21 @@ var path = require('path')
 module.exports = {
   input: function () { return path.join(__dirname,'./input/people.json') },
   output: function () { return path.join(__dirname,'./output/people.json') },
-  createReadStream: function (input) {
-    return fs.createReadStream(input)
+  createReadStream: function (inputPath) {
+    var stream =  fs.createReadStream(inputPath)
+
+    return stream
   },
-  createWriteStream: function (input, output) {
-    return input.pipe(fs.createWriteStream(output))
-  }
+  createWriteStream: function (inputStream, outputPath) {
+    var stream = fs.createWriteStream(outputPath)
+
+    inputStream.pipe(stream)
+
+    return stream
+  },
+  onend: function (obj, callback) {
+    obj.on('end', callback)
+  },
+  cb: function () { return function () { console.log('end') } }
 }
 
