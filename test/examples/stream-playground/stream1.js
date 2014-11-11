aa8da52a1ce4565e33f8188a0d5fdb26b16df0ae
fibo <casati_gianluca@yahoo.it>
2014-11-11 07:13:17 +0000

    added stream example

---
+++ test/examples/stream-playground/stream1.js
@@ -0,0 +1,21 @@
+
+var dflow = require('dflow')
+
+var graph = require('./stream.json')
+var funcs = require('./funcs')
+
+var f = dflow.fun(funcs, graph)
+
+console.log(graph)
+
+f()
+
+/*
+var fs = require("fs");
+
+// Read File
+fs.createReadStream("input/people.json")
+    // Write File
+    .pipe(fs.createWriteStream("output/people.json"));
+*/
+
