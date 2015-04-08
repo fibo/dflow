b515dfc06f36cc4bb79e70cc8f8086d54b7948d7
fibo <casati_gianluca@yahoo.it>
2015-04-08 08:34:45 -0400

    generic npm run build

--- dist/dflow.js
+++ dist/dflow.js
@@ -1,4 +1,4 @@
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.dflow=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.dflow = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
 //
 // Dependency graph
@@ -24,7 +24,13 @@ function typeofOperator (operand) { return typeof operand }
 
 exports['typeof'] = typeofOperator
 
-function method0 (obj, method) { return obj[method]() }
+//function method0 (obj, method) { return obj[method]() }
+
+//function method1 (obj, method, arg1) { return obj[method](arg1) }
+
+//function method2 (obj, method, arg1, arg2) { return obj[method](arg1, arg2) }
+
+//exports['method2'] = method2
 
 function applyMethod (fun, thisArg, argsArray) { return fun.apply(thisArg, argsArray) }
 
@@ -56,7 +62,7 @@ function division (a, b) { return a / b }
 
 exports['/'] = division
 
-// Logic operators.
+// Logical operators.
 
 function and (a, b) { return a && b }
 
