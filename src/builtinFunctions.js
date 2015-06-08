585933f2916dffe6d4b421837276d6a24e4ee7e1
Gianluca Casati <fibo@users.noreply.github.com>
2015-06-08 17:57:02 -0400

    more builtin operators

--- src/builtinFunctions.js
+++ src/builtinFunctions.js
@@ -1,55 +1,79 @@
 
-function typeofOperator (operand) { return typeof operand }
+// Special values.
 
-exports['typeof'] = typeofOperator
+function infinity () { return Infinity }
+exports['Infinity'] = infinity
 
-//function method0 (obj, method) { return obj[method]() }
-
-//function method1 (obj, method, arg1) { return obj[method](arg1) }
-
-//function method2 (obj, method, arg1, arg2) { return obj[method](arg1, arg2) }
-
-//exports['method2'] = method2
-
-function applyMethod (fun, thisArg, argsArray) { return fun.apply(thisArg, argsArray) }
-
-exports.apply = applyMethod
+function nan () { return NaN }
+exports['NaN'] = nan
 
 function nullValue () { return null }
-
 exports['null'] = nullValue
 
-function dot (obj, prop) { return obj[prop] }
+// Operators
 
+function typeofOperator (operand) { return typeof operand }
+exports['typeof'] = typeofOperator
+
+function applyMethod (fun, thisArg, argsArray) { return fun.apply(thisArg, argsArray) }
+exports.apply = applyMethod
+
+function dot (obj, prop) { return obj[prop] }
 exports['.'] = dot
 
 // Arithmetic operators.
 
 function addition (a, b) { return a + b }
-
 exports['+'] = addition
 
 function multiplication (a, b) { return a * b }
-
 exports['*'] = multiplication
 
 function subtraction (a, b) { return a - b }
-
 exports['-'] = subtraction
 
 function division (a, b) { return a / b }
-
 exports['/'] = division
+
+function modulus (a, b) { return a % b }
+exports['%'] = modulus
 
 // Logical operators.
 
 function and (a, b) { return a && b }
-
 exports['&&'] = and
 
 function or (a, b) { return a || b }
-
 exports['||'] = or
+
+function not (a) { return ! a }
+exports['!'] = not
+
+// Comparison operators.
+
+function equalTo (a, b) { return a == b }
+exports['=='] = equalTo
+
+function equalValueAndEqualType (a, b) { return a === b }
+exports['==='] = equalValueAndEqualType
+
+function notEqual (a, b) { return a != b }
+exports['!='] = notEqual
+
+function notEqualValueAndEqualType (a, b) { return a !== b }
+exports['!=='] = notEqualValueAndEqualType
+
+function greaterThen (a, b) { return a > b }
+exports['>'] = greaterThen
+
+function lessThen (a, b) { return a < b }
+exports['<'] = lessThen
+
+function greaterThenOrEqualTo (a, b) { return a >= b }
+exports['>='] = greaterThenOrEqualTo
+
+function lessThenOrEqualTo (a, b) { return a <= b }
+exports['<='] = lessThenOrEqualTo
 
 // console.
 
@@ -57,6 +81,91 @@ exports['console.log'] = console.log.bind(console)
 
 // Math.
 
-exports['Math.cos'] = Math.cos
-exports['Math.sin'] = Math.sin
+function MathE () { return Math.E }
+exports['Math.E'] = MathE
+
+function MathLN2 () { return Math.LN2 }
+exports['Math.LN2'] = MathLN2
+
+function MathLN10 () { return Math.LN10 }
+exports['Math.LN10'] = MathLN10
+
+function MathLOG2 () { return Math.LOG2 }
+exports['Math.LOG2'] = MathLOG2
+
+function MathLOG10 () { return Math.LOG10 }
+exports['Math.LOG10'] = MathLOG10
+
+function MathPI () { return Math.PI }
+exports['Math.PI'] = MathPI
+
+function MathSQRT1_2 () { return Math.SQRT1_2 }
+exports['Math.SQRT1_2'] = MathSQRT1_2
+
+function MathSQRT2 () { return Math.SQRT2 }
+exports['Math.SQRT2'] = MathSQRT2
+
+exports['Math.abs']    = Math.abs
+exports['Math.acos']   = Math.acos
+exports['Math.acosh']  = Math.acosh
+exports['Math.asin']   = Math.asin
+exports['Math.asinh']  = Math.asinh
+exports['Math.atan']   = Math.atan
+exports['Math.atanh']  = Math.atanh
+exports['Math.atan2']  = Math.atan2
+exports['Math.cbrt']   = Math.cbrt
+exports['Math.ceil']   = Math.ceil
+exports['Math.clz32']  = Math.clz32
+exports['Math.cos']    = Math.cos
+exports['Math.cosh']   = Math.cosh
+exports['Math.exp']    = Math.exp
+exports['Math.expm1']  = Math.expm1
+exports['Math.floor']  = Math.floor
+exports['Math.fround'] = Math.fround
+exports['Math.hypot']  = Math.hypot
+exports['Math.imul']   = Math.imul
+exports['Math.log']    = Math.log
+exports['Math.logip']  = Math.logip
+exports['Math.log10']  = Math.log10
+exports['Math.log2']   = Math.log2
+exports['Math.max']    = Math.max
+exports['Math.min']    = Math.min
+exports['Math.pow']    = Math.pow
+exports['Math.random'] = Math.random
+exports['Math.round']  = Math.round
+exports['Math.sign']   = Math.sign
+exports['Math.sin']    = Math.sin
+exports['Math.sinh']   = Math.sinh
+exports['Math.sqrt']   = Math.sqrt
+exports['Math.tan']    = Math.tan
+exports['Math.tanh']   = Math.tanh
+exports['Math.trunc']  = Math.trunc
+
+// JSON
+
+exports['JSON.parse']     = JSON.parse
+exports['JSON.stringify'] = JSON.stringify
+
+// String
+
+exports['String.prototype.charAt']            = String.prototype.charAt
+exports['String.prototype.charCodeAt']        = String.prototype.charCodeAt
+exports['String.prototype.concat']            = String.prototype.concat
+exports['String.prototype.indexOf']           = String.prototype.indexOf
+exports['String.prototype.lastIndexOf']       = String.prototype.lastIndexOf
+exports['String.prototype.repeat']            = String.prototype.repeat
+exports['String.prototype.search']            = String.prototype.search
+exports['String.prototype.slice']             = String.prototype.slice
+exports['String.prototype.split']             = String.prototype.split
+exports['String.prototype.substr']            = String.prototype.substr
+exports['String.prototype.substring']         = String.prototype.substring
+exports['String.prototype.toLocaleLowerCase'] = String.prototype.toLocaleLowerCase
+exports['String.prototype.toLocaleUpperCase'] = String.prototype.toLocaleUpperCase
+exports['String.prototype.toLowerCase']       = String.prototype.toLowerCase
+exports['String.prototype.toUpperCase']       = String.prototype.toUpperCase
+exports['String.prototype.trim']              = String.prototype.trim
+
+// Date
+
+exports['Date.now'] = Date.now
 
