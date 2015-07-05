9aaad4360abceeb117ccecc90cd1e490709b7412
Gianluca Casati <fibo@users.noreply.github.com>
2015-07-05 17:29:38 +0000

    updated deps tree

--- index.js
+++ index.js
@@ -3,15 +3,27 @@
 // Dependency graph
 //
 // fun.js
-// ├── injectAccessors.js
-// ├── injectArguments.js
-// ├── injectReferences.js
+// ├── builtinFunctions.js
+// ├── inject/accessors.js
+// │   └── regex/accessors.js
+// ├── inject/additionalFunctions.js
+// ├── inject/arguments.js
+// │   └── regex/arguments.js
+// ├── inject/dotOperator.js
+// │   └── regex/dotOperator.js
+// ├── inject/references.js
+// │   └── regex/references.js
 // ├── inputArgs.js
 // │   └── inputPipes.js
+// ├── isDflowFun.js
 // ├── level.js
 // │   └── parents.js
 // │       └── inputPipes.js
 // └── validate.js
+//     ├── regex/accessors.js
+//     ├── regex/arguments.js
+//     ├── regex/dotOperator.js
+//     └── regex/references.js
 //
 
 exports.fun = require('./src/fun')
