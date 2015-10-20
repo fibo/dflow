f103f79bdce2de98f17734927055a28789122221
Gianluca Casati <fibo@users.noreply.github.com>
2015-10-20 20:54:49 +0000

    more regex tests

--- test/regex.js
+++ test/regex.js
@@ -1,8 +1,26 @@
 
-var dotOperator = require('../src/engine/regex/dotOperator'),
+var accessor    = require('../src/engine/regex/accessor'),
+    argument    = require('../src/engine/regex/argument'),
+    dotOperator = require('../src/engine/regex/dotOperator'),
+    reference   = require('../src/engine/regex/reference'),
     should      = require('should')
 
 describe('regex', function () {
+  describe('accessor', function () {
+    it('matches @attributeName', function () {
+      accessor.test('@foo').should.be.true
+    })
+  })
+
+  describe('argument', function () {
+    it('matches arguments[N]', function () {
+      argument.test('arguments[0]').should.be.true
+      argument.test('arguments[1]').should.be.true
+      argument.test('arguments[2]').should.be.true
+      argument.test('arguments[3]').should.be.true
+    })
+  })
+
   describe('dotOperator.attr', function () {
     it('matches .validJavaScriptVariableName', function () {
       dotOperator.func.test('.foo').should.be.true
@@ -14,6 +32,12 @@ describe('regex', function () {
     it('matches .validJavaScriptFunctionName()', function () {
       dotOperator.func.test('.foo()').should.be.true
       dotOperator.func.test('.1foo()').should.be.false
+    })
+  })
+
+  describe('reference', function () {
+    it('matches &functionName', function () {
+      reference.test('&foo').should.be.true
     })
   })
 })
