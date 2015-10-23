e47e0dbc4520bb9503119d417dbef9a98c821fc0
fibo <casati_gianluca@yahoo.it>
2015-10-23 19:41:28 -0400

    added selectNode but undefined ):

--- js/examples-renderer.js
+++ js/examples-renderer.js
@@ -4933,7 +4933,7 @@ function selectNode (data) {
 
   var node = this.node[id]
 
-  canvas.nodeControls.attachTo(node)
+  this.nodeControls.attachTo(node)
 }
 
 Canvas.prototype.selectNode = selectNode
