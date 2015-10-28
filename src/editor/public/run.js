dd684c3513a084764840962f369df35c7fe43144
fibo <casati_gianluca@yahoo.it>
2015-10-28 17:50:10 -0400

    createParagraph example

--- src/editor/public/run.js
+++ src/editor/public/run.js
@@ -341,6 +341,20 @@ exports.AudioContext = function () {
   return window.AudioContext || window.webkitAudioContext
 }
 
+exports.getElementById = function (id) {
+  return window.document.getElementById(id)
+}
+
+exports.createElement = function (el) {
+  return window.document.createElement(el)
+}
+
+exports.innerHTML = function (node, content) {
+  node.innerHTML = content
+
+  return node
+}
+
 
 },{}],7:[function(require,module,exports){
 
