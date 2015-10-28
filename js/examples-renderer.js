70521694e421ac39d0369b88794fe77dbeef35c3
Gianluca Casati <fibo@users.noreply.github.com>
2015-10-28 15:34:08 -0400

    edit graph in memory

--- js/examples-renderer.js
+++ js/examples-renderer.js
@@ -6431,8 +6431,7 @@ exports.apply = applyMethod
 function dot (obj, prop) { return obj[prop] }
 exports['.'] = dot
 
-function typeofOperator (operand) { return typeof operand }
-exports['typeof'] = typeofOperator
+exports['typeof'] = function (a) { return typeof a }
 
 function newOperator () {
   var Obj = arguments[0],
@@ -6456,26 +6455,13 @@ exports['new'] = newOperator
 
 // Array
 
-function emptyArray () { return [] }
-exports['[]'] = emptyArray
-
-exports.isArray  = Array.isArray
+exports['[]'] = function () { return [] }
 
 exports.indexOf = function (a, b) { return a.indexOf(b) }
 
-exports.filter = function (a, b, t) {
-  if (typeof t === 'undefined')
-    return a.filter(c, t)
-  else
-    return a.filter(c)
-}
+exports.push = function (a, b) { return a.push(b) }
 
-exports.forEach = function (a, c) {
-  if (typeof t === 'undefined')
-    return a.forEach(c, t)
-  else
-    return a.forEach(c)
-}
+exports.pop = function (a, b) { return a.pop(b) }
 
 // console
 
@@ -6499,13 +6485,6 @@ exports['{}'] = function () { return {} }
 exports.false = function () { return false }
 
 exports.true = function () { return true }
-
-// Assignment
-
-exports['='] = function (a, b) {
-  a = b
-  return b
-}
 
 
 },{}],30:[function(require,module,exports){
