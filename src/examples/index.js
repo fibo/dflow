9773d8d6a7fc0c75630df60bf0a330b4f712885c
fibo <casati_gianluca@yahoo.it>
2016-01-29 18:03:20 -0500

    fixed example graphs

--- src/examples/index.js
+++ src/examples/index.js
@@ -1,7 +1,9 @@
+// Do not use dynamic imports, for example importing the whole graph folder;
+// use explicit imports instead, otherwise browserify will not include graphs.
 exports.apply = require('./graph/apply.json')
-exports.createParagraph = require('./createParagraph.json')
+exports.createParagraph = require('./graph/createParagraph.json')
 exports.dateParse = require('./graph/dateParse.json')
-exports.dotOperator = require('dotOperator.json')
+exports.dotOperator = require('./graph/dotOperator.json')
 exports['hello-world'] = require('./graph/hello-world.json')
 exports.indexOf = require('./graph/indexOf.json')
 exports['new'] = require('./graph/new.json')
