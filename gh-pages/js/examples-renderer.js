752f809faaa514a3b0cad00db021d924d5b7c9f6
Gianluca Casati <fibo@users.noreply.github.com>
2016-01-29 19:10:34 -0500

    try to fix apply example

--- gh-pages/js/examples-renderer.js
+++ gh-pages/js/examples-renderer.js
@@ -8278,7 +8278,7 @@ module.exports={
 },{}],63:[function(require,module,exports){
 // Do not use dynamic imports, for example importing the whole graph folder;
 // use explicit imports instead, otherwise browserify will not include graphs.
-exports.apply = require('./graph/apply.json')
+exports['apply'] = require('./graph/apply.json')
 exports.createParagraph = require('./graph/createParagraph.json')
 exports.dateParse = require('./graph/dateParse.json')
 exports.dotOperator = require('./graph/dotOperator.json')
