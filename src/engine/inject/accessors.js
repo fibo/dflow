e40f4224e0af9c4622b2ef79fd659207b35a6b0c
Gianluca Casati <fibo@users.noreply.github.com>
2016-01-31 16:52:49 -0500

    added this.graph.data closes #50

--- src/engine/inject/accessors.js
+++ src/engine/inject/accessors.js
@@ -1,7 +1,7 @@
 var accessorRegex = require('../regex/accessor')
 
 /**
- * Inject functions to set or get context keywords.
+ * Inject functions to set or get graph data.
  *
  * @api private
  *
@@ -13,6 +13,8 @@ function injectAccessors (funcs, graph) {
   if (typeof graph.data === 'undefined') {
     graph.data = {}
   }
+
+  funcs['this.graph.data'] = function () { return graph.data }
 
   /**
    * Inject accessor.
--- test/inject/accessors.js
+++ test/inject/accessors.js
@@ -1,17 +1,17 @@
 var injectAccessors = require('../../src/engine/inject/accessors')
+var should = require('should')
 
 describe('injectAccessors', function () {
   it('modifies funcs object with accessors injected', function () {
-    var data
     var funcs = {}
     var graph = {
       task: {
-        '1': '@foo',
-        '2': '@bar'
+        a: '@foo',
+        b: '@bar'
       },
       data: {
-        'foo': 1,
-        'bar': [2]
+        foo: 1,
+        bar: [2]
       }
     }
 
@@ -26,9 +26,35 @@ describe('injectAccessors', function () {
     foo().should.be.eql(graph.data.foo)
     bar().should.be.eql(graph.data.bar)
 
-    data = { a: [2, 3] }
+    var data = { a: [2, 3] }
     foo(data)
     foo().should.be.eql(graph.data.foo)
     foo().should.be.eql(data)
+  })
+
+  it('injects accessor this.graph.data', function () {
+    var funcs = {}
+    var graph = {
+      task: {
+        a: 'data'
+      },
+      data: {
+        foo: 1,
+        bar: [2]
+      }
+    }
+
+    injectAccessors(funcs, graph)
+
+    var data = funcs['this.graph.data']
+
+    should.deepEqual(data(), graph.data)
+
+    graph.data.quz = false
+    should.deepEqual(data(), {
+      foo: 1,
+      bar: [2],
+      quz: false
+    })
   })
 })
