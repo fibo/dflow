a89b77e97b797b5201c79ce9d2ada3dfa9de393b
Gianluca Casati <fibo@users.noreply.github.com>
2016-01-31 17:51:52 -0500

    added sidebar

--- gh-pages/js/examples-renderer.js
+++ gh-pages/js/examples-renderer.js
@@ -6779,7 +6779,7 @@ exports.innerHTML = function (node, content) {
 var accessorRegex = require('../regex/accessor')
 
 /**
- * Inject functions to set or get context keywords.
+ * Inject functions to set or get graph data.
  *
  * @api private
  *
@@ -6791,6 +6791,8 @@ function injectAccessors (funcs, graph) {
   if (typeof graph.data === 'undefined') {
     graph.data = {}
   }
+
+  funcs['this.graph.data'] = function () { return graph.data }
 
   /**
    * Inject accessor.
