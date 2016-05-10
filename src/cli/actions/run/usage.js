2e75dcb40eba6a21f241d8a8c2be825364f62d9a
fibo <casati_gianluca@yahoo.it>
2016-05-10 06:13:37 -0400

    added action

---
+++ src/cli/actions/run/usage.js
@@ -0,0 +1,6 @@
+module.exports = `
+Usage: dflow run [options] [path]
+
+Available options:
+  -h, --help          print usage and exit
+`
---
+++ src/cli/actions/serve/usage.js
@@ -0,0 +1,6 @@
+module.exports = `
+Usage: dflow serve [options] [path]
+
+Available options:
+  -h, --help          print usage and exit
+`
