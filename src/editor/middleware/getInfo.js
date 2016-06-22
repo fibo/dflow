f96cee84e8ffcf0caeeb3a36b636dc04670065ee
Gianluca Casati <fibo@users.noreply.github.com>
2016-06-22 16:05:40 -0400

    middleware test working

--- src/editor/middleware/getInfo.js
+++ src/editor/middleware/getInfo.js
@@ -4,9 +4,16 @@ var pkg = require('../../../package.json')
 var endpoint = '/info'
 
 function handler (req, res, next) {
+  if (req.method !== 'GET') {
+    next()
+    return
+  }
+
   if (url.parse(req.url).pathname === endpoint) {
     res.setHeader('Content-Type', 'application/json')
+
     res.statusCode = 200
+
     res.end(JSON.stringify({
       name: pkg.name,
       version: pkg.version
