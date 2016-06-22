91e33b22547d55a8ccc0ba6fa39bd99a70e32496
fibo <casati_gianluca@yahoo.it>
2016-06-22 16:05:40 -0400

    middleware test working

--- test/editor/middleware/getInfo.js
+++ test/editor/middleware/getInfo.js
@@ -1,17 +1,15 @@
-/*
 var request = require('supertest')
 var connect = require('connect')
 
-var getInfo = require('../../../src/editor/middleware/getInfo')
+var getInfo = require('editor/middleware/getInfo')
 var app = connect()
 
-app.use(getInfo.endpoint, getInfo.handler)
+app.use(getInfo.handler)
 
 var pkg = require('../../../package.json')
-*/
 
 describe('GET /info', function () {
-  it('returns package info'/*, function (done) {
+  it('returns package info', function (done) {
     request(app)
       .get('/info')
       .set('Accept', 'application/json')
@@ -20,5 +18,5 @@ describe('GET /info', function () {
         name: pkg.name,
         version: pkg.version
       }, done)
-  }*/)
+  })
 })
