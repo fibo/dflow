e1bff4ce3baae9d637dba0722865df57fa0332f1
fibo <casati_gianluca@yahoo.it>
2016-06-24 18:59:44 -0400

    added dflow run action

--- src/cli/actions/run/index.js
+++ src/cli/actions/run/index.js
@@ -1,5 +1,11 @@
 var nopt = require('nopt')
 var usage = require('./usage')
+var utils = require('../../utils')
+var no = require('not-defined')
+var fun = require('../../../engine/fun')
+
+var dotJson = utils.dotJson
+var appendCwd = utils.appendCwd
 
 var knownOpts = {
   help: Boolean
@@ -9,11 +15,43 @@ var shortHandOpts = {
   h: '--help'
 }
 
+const showUsage = () => {
+  console.log(usage)
+  process.exit(0)
+}
+
 module.exports = (args) => {
   var opt = nopt(knownOpts, shortHandOpts, args, 3)
 
-  if (opt.help) {
-    console.log(usage)
-    process.exit(0)
+  var remain = opt.argv.remain
+
+  if ((opt.help) || (remain.length === 0)) showUsage()
+
+  var graphPath = remain.filter(dotJson)
+                        .map(appendCwd)
+                        .shift()
+
+  if (no(graphPath)) showUsage()
+
+  try {
+    var graph = require(graphPath)
+
+    if (graph.info && graph.info.context === 'client') {
+      console.log('TODO: implement running client side, spawn a server...')
+      process.exit(0)
+    }
+
+    // TODO Arguments of dflowFun should be passed as --arg 2016 --arg 12 --arg 18
+    var dflowFunArgs = [2016, 12, 12]
+
+    // var funcs = {} // TODO additional functions
+    var f = fun(graph /* , funcs */)
+
+    // TODO remove the console.log below, it is there just to check it is
+    // working, try
+    // node src/cli/dflow run src/examples/graphs/new.json
+    console.log(f.apply(null, dflowFunArgs))
+  } catch (err) {
+    console.error(err)
   }
 }
