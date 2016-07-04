62bf51f0f6cb868de624b40ebacc81bfc095ef42
Gianluca Casati <fibo@users.noreply.github.com>
2016-07-04 13:17:11 +0200

    do not lint examples

--- src/examples/d3/rollup.config.js
+++ src/examples/d3/rollup.config.js
@@ -1,9 +1,9 @@
-import npm from "rollup-plugin-node-resolve";
+import npm from 'rollup-plugin-node-resolve'
 
 export default {
-  entry: "index.js",
-  format: "umd",
-  moduleName: "d3",
+  entry: 'index.js',
+  format: 'umd',
+  moduleName: 'd3',
   plugins: [npm({jsnext: true})],
-  dest: "d3.js"
-};
+  dest: 'd3.js'
+}
