8128c739c9476cf07fa5219cb98a4463a7e63138
fibo <casati_gianluca@yahoo.it>
2016-08-28 10:12:25 -0400

    first try witn FlowView container

--- src/editor/reducers/index.js
+++ src/editor/reducers/index.js
@@ -3,28 +3,17 @@ import {
   FETCH_GRAPH_REQUEST,
   FETCH_GRAPH_SUCCESS
 } from '../actions'
-import viewActions from 'flow-view/actions'
-import viewReducers from 'flow-view/reducers'
 
 import emptyGraph from '../../engine/emptyGraph.json'
-import viewInitialState from 'flow-view/util/initialState'
+import emptyView from 'flow-view/util/emptyView'
 
 const initialState = Object.assign({},
   emptyGraph,
-  { view: viewInitialState }
+  { view: emptyView }
 )
 
 export default function (state = initialState, action) {
-  let view = Object.assign({}, state.view)
-
-  if (viewActions.includes(action.type)) {
-    view = viewReducers(state.view, action)
-  }
-
   switch (action.type) {
-    case viewActions.ADD_NODE:
-      return state
-
     case FETCH_GRAPH_FAILURE:
       return state
 
@@ -35,6 +24,6 @@ export default function (state = initialState, action) {
       return Object.assign({}, state, action.graph)
 
     default:
-      return Object.assign({}, state, { view })
+      return state
   }
 }
