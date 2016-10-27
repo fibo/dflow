4363e68513068571497ed9ee676df77ce4cd012b
fibo <casati_gianluca@yahoo.it>
2016-10-27 23:18:51 +0200

    better server-side renderer

--- src/examples/renderer/server-side.js
+++ src/examples/renderer/server-side.js
@@ -1,14 +1,16 @@
 const Canvas = require('flow-view').Canvas
 const path = require('path')
+const write = require('write-file-utf8')
 
 const graphName = process.argv[2]
 
 const view = require(path.join(__dirname, '..', 'graphs', `${graphName}.json`)).view
+const svgFile = path.join(__dirname, '..', '..', '..', 'gh-pages', 'svg', `${graphName}.svg`)
 
 const canvas = new Canvas('drawing')
 
 canvas.render(view, null, function (err, outputSVG) {
   if (err) throw err
 
-  console.log(outputSVG)
+  write(svgFile, outputSVG)
 })
