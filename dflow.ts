91328c4004df8b179da35843d0695536ed42a2ae
fibo <casati_gianluca@yahoo.it>
2021-06-03 18:41:39 +0200

    can addNode

--- dflow.ts
+++ dflow.ts
@@ -1,27 +1,41 @@
+export type DflowId = string;
+
 export interface DflowNodeSerialized {
+  id: string;
   kind: string;
 }
 
 export class DflowNode {
+  readonly graph: DflowGraph;
+  readonly id: string;
   readonly kind: string;
 
-  constructor({ kind }: DflowNodeSerialized) {
+  constructor(graph: DflowGraph, { id, kind }: DflowNodeSerialized) {
+    this.graph = graph;
+    this.id = id;
     this.kind = kind;
   }
 
   toJSON(): string {
-    return JSON.stringify({ kind: this.kind });
+    return JSON.stringify({ id: this.id, kind: this.kind });
   }
 }
 
 export class DflowGraph {
-  readonly nodes = new Map<string, DflowNode>();
+  nodes: Map<DflowId, DflowNode> = new Map();
 
   toJSON(): string {
-    return JSON.stringify({ nodes: [], edges: [] });
+    const nodes = Object.values(this.nodes).map((node) => node.toJSON());
+
+    return JSON.stringify({ nodes, edges: [] });
   }
 }
 
 export class DflowHost {
   readonly graph = new DflowGraph();
+
+  addNode(nodeJson: DflowNodeSerialized) {
+    const node = new DflowNode(this.graph, nodeJson);
+    this.graph.nodes.set(node.id, node);
+  }
 }
--- dflow_test.ts
+++ dflow_test.ts
@@ -1,9 +1,20 @@
-import { assertObjectMatch } from "std/testing/asserts.ts";
+import {
+  assertEquals,
+  assertObjectMatch,
+} from "https://deno.land/std@0.97.0/testing/asserts.ts";
 
 import { DflowHost } from "./dflow.ts";
 
-Deno.test("Empty graph", () => {
+Deno.test("empty graph", () => {
   const dflow = new DflowHost();
-
   assertObjectMatch(JSON.parse(dflow.graph.toJSON()), { nodes: [], edges: [] });
+});
+
+Deno.test("addNode", () => {
+  const nodeId1 = "id1";
+  const dflow = new DflowHost();
+  dflow.addNode({ id: nodeId1, kind: "MyNode" });
+
+  const node1 = dflow.graph.nodes.get(nodeId1);
+  assertEquals(nodeId1, node1?.id);
 });
