4096ae1d970a5ad5046b77d013faf3aab3f6f4a9
fibo <casati_gianluca@yahoo.it>
2021-06-11 22:41:19 +0200

    check pin types at runtime

--- nodes/data-types.ts
+++ nodes/data-types.ts
@@ -1,7 +1,12 @@
-import { DflowAbstractZeroInOneOut } from "./abstract.ts";
+import { DflowNode, DflowSerializedNode } from "../engine.ts";
+import { outNum } from "./abstract.ts";
 
-export class DflowNum extends DflowAbstractZeroInOneOut {
+export class DflowNum extends DflowNode {
   static kind = "num";
+
+  constructor(arg: DflowSerializedNode) {
+    super({ ...arg, outputs: [outNum()] });
+  }
 
   run() {}
 }
