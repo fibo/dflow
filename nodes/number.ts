d80abc56c1858cd9a6448228c7ce559e08f4c0dd
Gianluca Casati <fibo@users.noreply.github.com>
2021-06-13 20:20:19 +0000

    add parseInt and parseFloat

--- nodes/number.ts
+++ nodes/number.ts
@@ -8,6 +8,14 @@ class DflowIsFinite extends DflowAbstractOneNumInOneBoolOut {
   }
 }
 
+class DflowIsInteger extends DflowAbstractOneNumInOneBoolOut {
+  static kind = "isInteger";
+
+  task(num: number) {
+    return Number.isInteger(num);
+  }
+}
+
 class DflowIsNaN extends DflowAbstractOneNumInOneBoolOut {
   static kind = "isNaN";
 
@@ -16,7 +24,26 @@ class DflowIsNaN extends DflowAbstractOneNumInOneBoolOut {
   }
 }
 
+class DflowParseFloat extends DflowAbstractOneNumInOneNumOut {
+  static kind = "parseFloat";
+
+  task(num: number) {
+    return Number.parseFloat(num);
+  }
+}
+
+class DflowParseInt extends DflowAbstractOneNumInOneNumOut {
+  static kind = "parseInt";
+
+  task(num: number) {
+    return Number.parseInt(num);
+  }
+}
+
 export const catalog = {
   [DflowIsFinite.kind]: DflowIsFinite,
+  [DflowIsInteger.kind]: DflowIsInteger,
   [DflowIsNaN.kind]: DflowIsNaN,
+  [DflowParseFloat.kind]: DflowParseFloat,
+  [DflowParseInt.kind]: DflowParseInt,
 };
--- nodes/number_test.ts
+++ nodes/number_test.ts
@@ -1,4 +1,4 @@
-import { testOneNumInOneBoolOut } from "./_test-utils.ts";
+import { testOneNumInOneBoolOut, testOneNumInOneNumOut } from "./_test-utils.ts";
 import { catalog } from "./number.ts";
 
 Deno.test(catalog.isFinite.kind, () => {
@@ -7,7 +7,23 @@ Deno.test(catalog.isFinite.kind, () => {
   testOneNumInOneBoolOut(nodeKind, Infinity, Number.isFinite(Infinity));
 });
 
+Deno.test(catalog.isInteger.kind, () => {
+  const nodeKind = catalog.isInteger.kind;
+  testOneNumInOneBoolOut(nodeKind, 1, Number.isInteger(1));
+  testOneNumInOneBoolOut(nodeKind, 1.5, Number.isInteger(1.5));
+});
+
 Deno.test(catalog.isNaN.kind, () => {
   const nodeKind = catalog.isNaN.kind;
   testOneNumInOneBoolOut(nodeKind, 1, Number.isNaN(1));
+});
+
+Deno.test(catalog.parseFloat.kind, () => {
+  const nodeKind = catalog.parseFloat.kind;
+  testOneNumInOneNumOut(nodeKind, 1.5, Number.parseFloat('1.5'));
+});
+
+Deno.test(catalog.parseInt.kind, () => {
+  const nodeKind = catalog.parseInt.kind;
+  testOneNumInOneNumOut(nodeKind, 1, Number.parseInt('1'));
 });
