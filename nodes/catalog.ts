f8bb48ddf41511357d923f146a5b6476d92c3234
Gianluca Casati <fibo@users.noreply.github.com>
2021-06-19 10:51:06 +0000

    add objectKeys node

--- nodes/catalog.ts
+++ nodes/catalog.ts
@@ -1,9 +1,11 @@
 import { catalog as dataTypesCatalog } from "./data-types.ts";
 import { catalog as mathCatalog } from "./math.ts";
 import { catalog as numberCatalog } from "./number.ts";
+import { catalog as objectCatalog } from "./object.ts";
 
 export const catalog = {
   ...dataTypesCatalog,
   ...mathCatalog,
   ...numberCatalog,
+  ...objectCatalog,
 };
