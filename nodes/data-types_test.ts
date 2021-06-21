0a1b37037875dc545bd1800fd45fc1227629dd14
fibo <casati_gianluca@yahoo.it>
2021-06-22 00:01:15 +0200

    dummy arguments test

--- nodes/data-types_test.ts
+++ nodes/data-types_test.ts
@@ -3,6 +3,17 @@ import { assertEquals } from "std/testing/asserts.ts";
 import { DflowHost } from "../dflow.ts";
 import { catalog } from "./data-types.ts";
 
+Deno.test(catalog.boolean.kind, () => {
+  const nodeKind = catalog.boolean.kind;
+
+  const bool = true;
+  const dflow = new DflowHost(catalog);
+  const testNode = dflow.newNode({ kind: nodeKind });
+  assertEquals(typeof testNode.getOutputByPosition(0).data, "undefined");
+  testNode.getOutputByPosition(0).data = bool;
+  assertEquals(testNode.getOutputByPosition(0).data, bool);
+});
+
 Deno.test(catalog.number.kind, () => {
   const nodeKind = catalog.number.kind;
 
@@ -12,4 +23,15 @@ Deno.test(catalog.number.kind, () => {
   assertEquals(typeof testNode.getOutputByPosition(0).data, "undefined");
   testNode.getOutputByPosition(0).data = num;
   assertEquals(testNode.getOutputByPosition(0).data, num);
+});
+
+Deno.test(catalog.string.kind, () => {
+  const nodeKind = catalog.string.kind;
+
+  const str = "foo";
+  const dflow = new DflowHost(catalog);
+  const testNode = dflow.newNode({ kind: nodeKind });
+  assertEquals(typeof testNode.getOutputByPosition(0).data, "undefined");
+  testNode.getOutputByPosition(0).data = str;
+  assertEquals(testNode.getOutputByPosition(0).data, str);
 });
