061091bf9728440e8bbd57ed2d8b9f732a1f065b
fibo <casati_gianluca@yahoo.it>
2021-06-26 04:09:05 +0200

    implement DflowFunction

--- foo.ts
+++
@@ -1,8 +0,0 @@
-import { DflowHost } from "./engine.ts";
-import { catalog } from "./nodes/catalog.ts";
-
-const dflow = new DflowHost(catalog);
-
-const fun = dflow.newNode({ kind: catalog.function.kind });
-
-console.log(fun.output(0).data);
