0929dc4231a743fde8719443829b41491ab9d932
Gianluca Casati <fibo@users.noreply.github.com>
2022-04-09 17:32:45 +0200

    add Date.now()

--- nodes/catalog.ts
+++ nodes/catalog.ts
@@ -1,6 +1,7 @@
 import { catalog as arrayCatalog } from "./array.ts";
 import { catalog as conditionalCatalog } from "./conditional.ts";
 import { catalog as consoleCatalog } from "./console.ts";
+import { catalog as dateCatalog } from "./date.ts";
 import { catalog as dataTypesCatalog } from "./data-types.ts";
 import { catalog as dflowCatalog } from "./dflow.ts";
 import { catalog as logicCatalog } from "./logic.ts";
@@ -15,6 +16,7 @@ export const catalog = {
   ...conditionalCatalog,
   ...consoleCatalog,
   ...dataTypesCatalog,
+  ...dateCatalog,
   ...dflowCatalog,
   ...logicCatalog,
   ...mathCatalog,
