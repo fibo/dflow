ab3956b010a943d4c2f12cb659ff4b00825dd30b
fibo <fibo@users.noreply.github.com>
2024-02-12 21:13:18 +0100

    resolve deno deprecation warnings

--- examples/nodes/conditional_test.ts
+++ examples/nodes/conditional_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "std/testing/asserts.ts";
+import { assertEquals } from "std/assert/mod.ts";
 import { DflowData } from "dflow";
 import { newDflow } from "./_test-utils.ts";
 
--- examples/nodes/date_test.ts
+++ examples/nodes/date_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "std/testing/asserts.ts";
+import { assertEquals } from "std/assert/mod.ts";
 import { newDflow } from "./_test-utils.ts";
 import { nodesCatalog as catalog } from "./index.ts";
 
